78330efa05f9436df59c01ac55e7b0df7627d14d
Ciaran Cleland <cleland16@googlemail.com>
2024-05-22 18:22:13 +0100

    Developed component communication & API integration

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,21 +1,67 @@
-import SearchBar from '../searchBar/searchBar'
 import './App.css';
+import React, {useState, useCallback} from 'react'
 
-function App() {
+import Spotify from '../../utilities/Spotify'
+import SearchBar from '../searchBar/searchBar'
+import Playlist from '../playlist/playlist'
+import SearchResults from '../searchResults/searchResult';
+
+const App = () => {
+  const [searchResults, setSearchResults] = useState([]);
+  const [playlistName, setPlaylistName] = useState('New Playlist');
+  const [playlistTracks, setPlaylistTracks] = useState([]);
+
+  const search = useCallback((term) => {
+    Spotify.search(term).then(setSearchResults);
+  }, []);
+
+  const addTrack = useCallback(
+    (track) => {
+      if (playlistTracks.some((savedTrack) => savedTrack.id === track.id))
+        return;
+      setPlaylistTracks((prevTracks) => [...prevTracks, track]);
+    }, 
+    [playlistTracks]
+  )
+
+  const removeTrack = useCallback(
+    (track) => {
+      setPlaylistTracks((prevTracks) => 
+        prevTracks.filter((currentTrack) => currentTrack.id !== track.id)
+    );
+  }, [])
+
+  const updatePlaylistName = useCallback((name) => {
+    setPlaylistName(name);
+  }, []);
+
+  const savePlaylist = useCallback(() => {
+    const trackUris = playlistTracks.map((track) => track.uri);
+    Spotify.savePlaylist(playlistName, trackUris).then(() => {
+      setPlaylistTracks([])
+    });
+  }, [playlistName, playlistTracks]);
+
   return (
     <div>
       <h1>
-        Spotify Playlist Creator
+      Spotify Playlist Creator
       </h1>
-      <SearchBar/>
-      
-      <section id='trackListSection'>
-      </section>
-      
-      <section is='selectedTracks'>
-      </section>
+      <div className="App">
+        <SearchBar onSearch={search}/>
+        <div className="App-playlist">
+          <SearchResults searchResults={searchResults} onAdd={addTrack} />
+          <Playlist 
+          playlistName={playlistName}
+          playlistTracks={playlistTracks}
+          onNameChange={updatePlaylistName}
+          onRemove={removeTrack}
+          onSave={savePlaylist}
+          />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default App
